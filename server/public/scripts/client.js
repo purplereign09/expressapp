f24efdb84e36bc23c52aa8d1c4dae5e773cab6a2
Patty Baraibar <pattybaraibar@msn.com>
2021-07-28 17:37:00 -0500

    updated files with AJAX

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,4 +5,34 @@ function onReady(){
 
     $('img').hide().fadeIn(5000);
 
+    //On page load, grab quote data from the server
+    getQuotes();
+}
+
+function getQuotes(){
+    //Ajax
+    //Making an HTTP request to our server
+    //to our GET/Quotes
+    $.ajax({
+        //Tell AJAX what endpoint to hit
+        //endpoint = url + method
+        method: 'GET',
+        url: '/quotes'
+    })
+        .then(function(response) {
+            console.log('GET /quotes response', response);
+            //render quotes w/jquery
+
+            let quoteList = $('#quotes');
+            console.log('quotes list element', quoteList);
+
+            for(let quote of response) {
+                quoteList.append(`
+                    <li>
+                        ${quote.text}
+                        <em> -- by ${quote.author}</em>
+                    </li>
+                `);
+            }
+        });
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,28 @@
 console.log(`hi patty!`);
 
-const express = require('express');
 
+
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+const quotes = [
+    {
+        text: 'Debuggin is like being the detective in a crime movie where you are also the murderer',
+        author: 'Felipe Fortes',
+    },
+    {
+        text: 'If you want to increase your success rate, double your failure rate',
+        author: 'Thomas Watson Jr.'
+    },
+    {
+        text: 'Code is there to explain the comments to the computer',
+        author: 'Andy Harris'
+    }
+];
 
 //Tell express where to find all
 //of our 'public' files
@@ -16,3 +36,36 @@ app.listen(5000, function(){
     //kind of like our 'onReady' function
     console.log('app is up and running');
 });
+
+app.use(express.static('./server/server.js'));
+
+
+//a specific URL: http://localhost:5000/trains
+app.get('/quotes', function(req, res){
+    console.log('Ready to send back some quotes');
+    console.log('request.route.path', req.route.path);
+
+    //send back data to client
+    res.send(quotes);
+});
+
+//POST /quotes
+app.post('/quotes', function(req, res){
+    
+
+    //body parser
+    //this is an OBJECT
+    console.log('req.body', req.body);
+    let newQuote = req.body;
+    quotes.push(newQuote);
+    
+    //Validate code
+    if(!newQuote.author || !newQuote.text){
+
+    }
+    res.sendStatus(400).send({
+       
+    });//created
+     return;
+});
+
